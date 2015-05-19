dfd5c11dff3f6a74d6251bb979d2f58565d338bf
joshi.rohit100 <joshi.rohit100@2504832.no-reply.drupal.org>
2015-05-19 20:22:54 +1000

    Issue #2491207 by joshi.rohit100: CAPTCHA ESLint errors

--- captcha.js
+++ captcha.js
@@ -6,6 +6,7 @@
  */
 
 (function ($) {
+  "use strict";
   Drupal.behaviors.captchaAdmin = {
     attach: function (context) {
       // Add onclick handler to checkbox for adding a CAPTCHA description
@@ -13,7 +14,7 @@
       // when no description should be added.
       // @todo: div.form-item-captcha-description depends on theming, maybe
       // it's better to add our own wrapper with id (instead of a class).
-      $("#edit-captcha-add-captcha-description").click(function() {
+      $("#edit-captcha-add-captcha-description").click(function () {
         if ($("#edit-captcha-add-captcha-description").is(":checked")) {
           // Show the CAPTCHA description textfield(s).
           $("div.form-item-captcha-description").show('slow');
--- image_captcha/image_captcha.js
+++ image_captcha/image_captcha.js
@@ -5,13 +5,14 @@
  * @TODO remove and use native states.
  */
 
-(function($) {
+(function ($) {
+  "use strict";
 
   Drupal.behaviors.captchaAdmin = {
-    attach : function(context) {
+    attach: function (context) {
 
       // Helper function to show/hide noise level widget.
-      var noise_level_shower = function(speed) {
+      var noise_level_shower = function (speed) {
         speed = (typeof speed == 'undefined') ? 'slow' : speed;
         if ($("#edit-image-captcha-dot-noise").is(":checked")
             || $("#edit-image-captcha-line-noise").is(":checked")) {
@@ -29,7 +30,7 @@
       noise_level_shower(0);
 
       // Helper function to show/hide smooth distortion widget.
-      var smooth_distortion_shower = function(speed) {
+      var smooth_distortion_shower = function (speed) {
         speed = (typeof speed == 'undefined') ? 'slow' : speed;
         if ($("#edit-image-captcha-distortion-amplitude").val() > 0) {
           $(".form-item-image-captcha-bilinear-interpolation").show(speed);
@@ -43,7 +44,7 @@
       $("#edit-image-captcha-distortion-amplitude").change(
           smooth_distortion_shower);
       // Show or hide appropriately on page load.
-      smooth_distortion_shower(0)
+      smooth_distortion_shower(0);
 
     }
   };
